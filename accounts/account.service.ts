5c059eb9bfb526e3bd7b00444cca533cf25d0bc2
Vaugn Xhander A. Garcia <xhander.garcia@gmail.com>
2026-05-12 01:42:17 +0800

    Add backend homepage route

--- accounts/account.service.ts
+++ accounts/account.service.ts
@@ -278,7 +278,7 @@ function basicDetails(account: any) {
     async function sendVerificationEmail(account: any, origin: any) {
     const frontendUrl = process.env.CORS_ORIGIN || 'http://localhost:4200';
 
-    const verifyUrl = `${frontendUrl}/account/verify-email?token=${account.verificationToken}`;
+    const verifyUrl = `${process.env.FRONTEND_URL}/account/verify-email?token=${account.verificationToken}`;
 
     const message = `
         <p>Please click the link below to verify your email address:</p>
