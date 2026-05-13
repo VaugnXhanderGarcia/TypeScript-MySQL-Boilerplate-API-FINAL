2c9cfc4a1a85e441739ea4399e4c6ad3b100afa8
Vaugn Xhander A. Garcia <xhander.garcia@gmail.com>
2026-05-13 21:00:42 +0800

    fixed the accounts cant log in

--- accounts/account.model.ts
+++ accounts/account.model.ts
@@ -1,33 +1,81 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes, Sequelize } from 'sequelize';
 
-export default function model(sequelize: any) {
-  const attributes = {
-    title: { type: DataTypes.STRING, allowNull: false },
-    firstName: { type: DataTypes.STRING, allowNull: false },
-    lastName: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    passwordHash: { type: DataTypes.STRING, allowNull: false },
-    acceptTerms: { type: DataTypes.BOOLEAN, allowNull: false },
-    role: { type: DataTypes.STRING, allowNull: false },
-    verificationToken: { type: DataTypes.STRING },
-    verified: { type: DataTypes.DATE },
-    resetToken: { type: DataTypes.STRING },
-    resetTokenExpires: { type: DataTypes.DATE },
-    passwordReset: { type: DataTypes.DATE },
-    created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
-    updated: { type: DataTypes.DATE }
-  };
+export default model;
 
-  const options = {
-    defaultScope: {
-      attributes: { exclude: ['passwordHash'] }
-    },
-    scopes: {
-      withHash: {
-        attributes: {}
-      }
-    }
-  };
+function model(sequelize: Sequelize) {
+    const attributes = {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        passwordHash: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        verificationToken: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        verified: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        resetToken: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        resetTokenExpires: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        passwordReset: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        updated: {
+            type: DataTypes.DATE,
+            allowNull: true
+        }
+    };
 
-  return sequelize.define('Account', attributes, options);
+    const options = {
+        tableName: 'accounts',
+        freezeTableName: true,
+        timestamps: false,
+        defaultScope: {
+            attributes: {
+                exclude: ['passwordHash']
+            }
+        },
+        scopes: {
+            withHash: {
+                attributes: {
+                    include: ['passwordHash']
+                }
+            }
+        }
+    };
+
+    return sequelize.define('Account', attributes, options);
 }
--- accounts/refresh-token.model.ts
+++ accounts/refresh-token.model.ts
@@ -1,40 +1,47 @@
-import { DataTypes, Sequelize } from 'sequelize';
+import { Sequelize, DataTypes } from 'sequelize';
 
 export default function RefreshTokenModel(sequelize: Sequelize) {
-    const attributes = {
-        token: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        expires: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        created: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW
-        },
-        createdByIp: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        revoked: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        revokedByIp: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        replacedByToken: {
-            type: DataTypes.STRING,
-            allowNull: true
-        }
-    };
+  const attributes = {
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    expires: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    createdByIp: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    revoked: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    revokedByIp: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    replacedByToken: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    accountId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  };
 
-    return sequelize.define('RefreshToken', attributes, {
+  const options = {
     tableName: 'refresh_tokens',
-    timestamps: true
-});
+    freezeTableName: true,
+    timestamps: false
+  };
+
+  return sequelize.define('RefreshToken', attributes, options);
 }
