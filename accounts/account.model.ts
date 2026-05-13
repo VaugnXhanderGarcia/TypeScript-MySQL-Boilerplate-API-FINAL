80263c3b25e8fcb494da73ab1d8456682727bfb6
Vaugn Xhander A. Garcia <xhander.garcia@gmail.com>
2026-05-13 20:09:23 +0800

    fix the acount login issue

--- accounts/account.model.ts
+++ accounts/account.model.ts
@@ -1,65 +1,24 @@
 import { DataTypes } from 'sequelize';
 
-export default function AccountModel(sequelize: any) {
-  const Account = sequelize.define('Account', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    passwordHash: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    acceptTerms: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
-    },
-    role: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    verificationToken: {
-      type: DataTypes.STRING
-    },
-    verified: {
-      type: DataTypes.DATE
-    },
-    resetToken: {
-      type: DataTypes.STRING
-    },
-    resetTokenExpires: {
-      type: DataTypes.DATE
-    },
-    passwordReset: {
-      type: DataTypes.DATE
-    },
-    created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW
-    },
-    updated: {
-      type: DataTypes.DATE
-    }
-  }, {
+export default function model(sequelize: any) {
+  const attributes = {
+    title: { type: DataTypes.STRING, allowNull: false },
+    firstName: { type: DataTypes.STRING, allowNull: false },
+    lastName: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    passwordHash: { type: DataTypes.STRING, allowNull: false },
+    acceptTerms: { type: DataTypes.BOOLEAN, allowNull: false },
+    role: { type: DataTypes.STRING, allowNull: false },
+    verificationToken: { type: DataTypes.STRING },
+    verified: { type: DataTypes.DATE },
+    resetToken: { type: DataTypes.STRING },
+    resetTokenExpires: { type: DataTypes.DATE },
+    passwordReset: { type: DataTypes.DATE },
+    created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+    updated: { type: DataTypes.DATE }
+  };
+
+  const options = {
     defaultScope: {
       attributes: { exclude: ['passwordHash'] }
     },
@@ -68,7 +27,7 @@ export default function AccountModel(sequelize: any) {
         attributes: {}
       }
     }
-  });
+  };
 
-  return Account;
+  return sequelize.define('Account', attributes, options);
 }
